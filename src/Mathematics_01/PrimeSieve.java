2f2f0200e8dbaf340cdbc9e8d8c5fe7e9610df3a
satishgupta07 <satishgupta.cse01@gmail.com>
2020-06-21 00:48:49 +0530

    Sieve of Eratosthenes
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- src/Mathematics_01/PrimeSieve.java
+++ src/Mathematics_01/PrimeSieve.java
@@ -5,27 +5,28 @@ import java.util.Scanner;
 public class PrimeSieve {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        isPrime(num);
+        int n = sc.nextInt();
+
+        sieveOfEratosthenes(n);
     }
 
-    public static boolean isPrime(int num){
-        if(num <= 0){
-            return false;
+    public static void sieveOfEratosthenes(int n){
+        boolean prime[] = new boolean[n+1];
+        for (int i = 0; i < n; i++) {
+            prime[i] = true ;
         }
-        if(num == 1){
-            return false;
-        }
-        if((num & 1) == 0){
-            return false;
-        }
-
-        boolean isPrime = true;
-        for (int i = 3; i*i <= num ; i+=2) {
-            if(num % i == 0){
-                isPrime = false;
+        for (int p = 2; p*p <=n ; p++) {
+            if(prime[p] == true){
+                for (int i = p*p ; i <= n ; i = i + p) {
+                     prime[i] = false;
+                }
             }
         }
-        return isPrime;
+
+        for (int i = 2; i <= n ; i++) {
+            if (prime[i] == true){
+                System.out.print(i+", ");
+            }
+        }
     }
 }
