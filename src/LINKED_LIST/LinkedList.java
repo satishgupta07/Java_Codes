7a204f2e0f612d1b673e60decbfe54028f2dd641
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-24 11:02:58 +0530

    LinkedList 0.2
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- src/LINKED_LIST/LinkedList.java
+++ src/LINKED_LIST/LinkedList.java
@@ -1,5 +1,7 @@
 package LINKED_LIST;
 
+import java.util.Stack;
+
 public class LinkedList {
 
     private Node head;
@@ -154,6 +156,260 @@ public class LinkedList {
         }
         return null;
     }
+
+    // Reverse LinkedList
+
+    public void reverseData(){
+        int left = 0;
+        int right = size -1;
+
+        while (left < right){
+            Node ln = get(left);
+            Node rn = get(right);
+
+            int temp = ln.value;
+            ln.value = rn.value;
+            rn.value = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public void reversePointers(){
+        Node prev = head;
+        Node curr = prev.next;
+
+        while (curr != null){
+            Node ahead = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = ahead;
+        }
+
+        // swap
+        Node t = head;
+        head = tail;
+        tail = t;
+
+        tail.next = null;
+    }
+
+    public void reverse(){
+        reverse(head);
+    }
+
+    private void reverse(Node node) {
+        if(node == tail){
+            head = tail;
+            return;
+        }
+        reverse(node.next);
+
+        tail.next = node;
+        tail = node;
+        tail.next = null;
+    }
+
+
+    public int mid(){
+        Node slow = head;
+        Node fast = head;
+
+        while (fast.next!=null && fast.next.next != null ){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.value;
+    }
+
+    public LinkedList merge(LinkedList first, LinkedList second) {
+        Node f = first.head;
+        Node s = second.head;
+
+        LinkedList res = new LinkedList();
+
+        while (f!=null && s!=null){
+            if(f.value < s.value){
+                res.insertlast(f.value);
+                f = f.next;
+            }
+            else {
+                res.insertlast(s.value);
+                s = s.next;
+            }
+        }
+        while (f!=null){
+            res.insertlast(f.value);
+            f=f.next;
+        }
+        while (s!=null){
+            res.insertlast(s.value);
+            s=s.next;
+        }
+
+        return res;
+    }
+
+    private Node midnode(){
+        Node slow = head;
+        Node fast = head;
+
+        while (fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public LinkedList mergesort(LinkedList list){
+        if(list.size == 1){
+            return list;
+        }
+        Node mid = list.midnode();
+
+        LinkedList first = new LinkedList();
+        first.head = list.head;
+        first.tail = mid ;
+        first.size = (list.size+1)/2;
+
+        LinkedList second = new LinkedList();
+        second.head = mid.next;
+        second.tail = tail;
+        second.size = (list.size)/2;
+
+        mid.next = null;
+
+        first = mergesort(first);
+        second = mergesort(second);
+
+        return merge(first,second);
+    }
+
+    public LinkedList oddeven(LinkedList list) {
+        LinkedList odd = new LinkedList();
+        LinkedList even = new LinkedList();
+        Node temp = list.head;
+        while (temp != null) {
+            if (temp.value % 2 == 0) {
+                even.insertlast(temp.value);
+            } else {
+                odd.insertlast(temp.value);
+            }
+            temp = temp.next;
+        }
+        odd.tail.next = even.head;
+        odd.tail = even.tail;
+        return odd;
+    }
+
+    public void oddeven(){
+
+        Node odd_head = null;
+        Node odd_tail = null;
+
+        Node even_head = null;
+        Node even_tail= null;
+
+        Node temp = head;
+
+        while(temp!=null){
+
+            if(temp.value % 2 != 0){
+
+                if(odd_head==null){
+
+                    odd_head= temp;
+                    odd_tail=temp;
+                }
+                else{
+                    odd_tail.next=temp;
+                    odd_tail=temp;
+                }
+            }
+
+            else{
+
+                if(even_head==null){
+
+                    even_head= temp;
+                    even_tail=temp;
+                }
+                else{
+                    even_tail.next=temp;
+                    even_tail=temp;
+                }
+            }
+
+            temp = temp.next;
+
+        }
+
+        odd_tail.next =even_head;
+        head = odd_head;
+
+        even_tail.next =null;
+        tail =even_tail;
+    }
+
+    public int kthfromlast(int k){
+
+        Node slow = head;
+        Node fast = head;
+
+
+        for (int i = 0; i <k; i++) {
+            fast = fast.next;
+        }
+
+        while(fast!=null){
+
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow.value;
+    }
+
+    public void kreverse(int k){
+
+        Node prev = null;
+
+        Stack<Node> stack = new Stack<>();
+
+        Node temp= head;
+
+        while(temp!=null){
+
+            int cnt =1;
+
+            while(temp!=null && cnt <k){
+
+                stack.push(temp);
+                temp =temp.next;
+                cnt=cnt+1;
+            }
+
+            while (!stack.isEmpty()){
+
+                if(prev == null){
+                    prev = stack.pop();
+                    head = prev;
+                }
+
+                else{
+
+                    prev.next=stack.pop();
+                    prev = prev.next;
+                }
+            }
+        }
+
+        prev.next=null;
+        tail = prev;
+    }
+
 
     public void display(){
 
--- src/LINKED_LIST/LinkedListClient.java
+++ src/LINKED_LIST/LinkedListClient.java
@@ -2,45 +2,97 @@ package LINKED_LIST;
 
 public class LinkedListClient {
     public static void main(String[] args) {
-        LinkedList list  = new LinkedList();
+//        LinkedList list  = new LinkedList();
+//
+//        list.insertfirst(1);
+//        list.insertfirst(2);
+//        list.insertfirst(3);
+//
+//        list.insertlast(4);
+//        list.insertlast(5);
+//        list.insertlast(6);
+//
+//        list.display();
+////
+////        System.out.println(list.get(3));
+////
+////        System.out.println(list.deletefirst());
+////
+////        System.out.println(list.deletelast());
+////
+////        list.display();
+////        System.out.println();
+////
+////        list.insertbetween(2,7);
+////
+////        list.display();
+////        System.out.println();
+////        System.out.println(list.deletebetween(3));
+////
+////        list.display();
+////
+////        list.insertfirst(8);
+////        list.insertfirst(8);
+////        System.out.println();
+////        list.display();
+////
+////        list.removeduplicates();
+////        System.out.println();
+////        list.display();
+////        System.out.println();
+////        System.out.println(list.find(1));
+////        System.out.println(list.find(9));
+//
+//        list.reverseData();
+//        list.display();
+//
+//        list.reversePointers();
+//        list.display();
+//
+//        list.reverse();
+//        list.display();
+//
+//        System.out.println(list.mid());
 
-        list.insertfirst(1);
-        list.insertfirst(2);
-        list.insertfirst(3);
+//        LinkedList list1 = new LinkedList();
+//        list1.insertlast(2);
+//        list1.insertlast(8);
+//        list1.insertlast(5);
+//
+//        LinkedList list2 = new LinkedList();
+//        list2.insertlast(9);
+//        list2.insertlast(7);
+//        list2.insertlast(4);
+//
+//        LinkedList res = new LinkedList();
+//        res = res.merge(list1,list2);
+//        res.display();
 
+        LinkedList list = new LinkedList();
+        list.insertlast(2);
+        list.insertlast(8);
         list.insertlast(4);
+        list.insertlast(9);
         list.insertlast(5);
-        list.insertlast(6);
+        list.insertlast(3);
+        list.insertlast(1);
+        list.insertlast(11);
+
 
         list.display();
 
-        System.out.println(list.get(3));
-
-        System.out.println(list.deletefirst());
-
-        System.out.println(list.deletelast());
-
-        list.display();
-        System.out.println();
-
-        list.insertbetween(2,7);
-
-        list.display();
-        System.out.println();
-        System.out.println(list.deletebetween(3));
-
+        list = list.mergesort(list);
         list.display();
 
-        list.insertfirst(8);
-        list.insertfirst(8);
-        System.out.println();
+        list = list.oddeven(list);
         list.display();
 
-        list.removeduplicates();
-        System.out.println();
+        list.oddeven();
         list.display();
-        System.out.println();
-        System.out.println(list.find(1));
-        System.out.println(list.find(9));
+
+        System.out.println(list.kthfromlast(3));
+
+        list.kreverse(3);
+        list.display();
     }
 }
