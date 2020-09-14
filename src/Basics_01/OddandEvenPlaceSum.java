5e6d234615075993e2241fe02624c836a88b1ddb
satishgupta07 <satishgupta.cse01@gmail.com>
2020-09-14 18:42:31 +0530

    update basic_01
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- src/Basics_01/OddandEvenPlaceSum.java
+++ src/Basics_01/OddandEvenPlaceSum.java
@@ -2,24 +2,58 @@ package Basics_01;
 
 public class OddandEvenPlaceSum {
     public static void main(String[] args) {
-        int num = 54873;
-        int OddSum = 0;
-        int EvenSum = 0;
-        int flag = 1;
+        int num = 234567;
+        getSum(num);
+    }
+//
+//    public static int reverse(int n) {
+//        int rev = 0;
+//        while (n != 0) {
+//            rev = (rev * 10) + (n % 10);
+//            n /= 10;
+//        }
+//        return rev;
+//    }
+//
+//    public static void getSum(int num){
+//        num = reverse(num);
+//        int OddSum = 0;
+//        int EvenSum = 0;
+//        boolean odd = true;
+//        while(num > 0){
+//            if(odd){
+//                OddSum += num % 10;
+//            }
+//            else {
+//                EvenSum += num % 10 ;
+//            }
+//
+//            num = num/10;
+//            odd = !odd;
+//        }
+//
+//        System.out.println("Sum of odd placed digits = " + OddSum);
+//        System.out.println("Sum of even placed digits = " + EvenSum);
+//    }
 
-        while(num > 0){
-            if(flag % 2 == 0){
-                EvenSum += num % 10 ;
-                num = num/10;
+    public static void getSum(int n){
+        int oddSum = 0;
+        int evenSum = 0;
+
+        boolean isOdd = (n % 2 == 1) ? true : false ;
+
+        while (n>0){
+            if(isOdd){
+                oddSum += n%10;
             }
-            else {
-                OddSum += num % 10;
-                num = num/10;
+            else{
+                evenSum += n%10;
             }
-            flag = flag +1;
+            isOdd = !isOdd;
+            n = n/10;
         }
+        System.out.println("Sum of odd placed digits = " + oddSum);
+        System.out.println("Sum of even placed digits = " + evenSum);
 
-        System.out.println("Sum of odd placed digits = " + OddSum);
-        System.out.println("Sum of even placed digits = " + EvenSum);
     }
 }
--- src/Basics_01/PerfectSquare.java
+++ src/Basics_01/PerfectSquare.java
@@ -3,16 +3,25 @@ package Basics_01;
 public class PerfectSquare {
     public static void main(String[] args) {
         int n = 81;
-        checkperfectsq(n);
+
+        if (isPerfectSquare(n)) {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+
     }
 
-    public static void checkperfectsq(int n){
-        for (int i = 1; i < n/5 ; i++) {
-            if(i*i == n){
-                System.out.println("Given number is perfect square of "+i);
-                return;
+    public static boolean isPerfectSquare(int n)
+    {
+        for (int i = 1; i * i <= n; i++) {
+
+            // If (i * i = n)
+            if ((n % i == 0) && (n / i == i)) {
+                return true;
             }
         }
-        System.out.println("Not a perfect square ");
+        return false;
     }
 }
